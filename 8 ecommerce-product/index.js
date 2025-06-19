560b4d996a358f48e2645c5b62ccf2d1969351fe
Temesgen Belachew <temesgenb439@gmail.com>
2025-06-19 18:00:16 +0300

    added  Select an element by its ID and class

---
+++ 8 ecommerce-product/index.js
@@ -0,0 +1,15 @@
+// Select an element by its ID
+const mainImg = document.getElementById("main-img");
+const mainPopupImg = document.getElementById("mainPopupImg");
+const thumbnails = Array.from(document.getElementsByClassName("thumbnail"));
+const popupThumbnails = Array.from(document.getElementsByClassName("popup-thumbnail"));
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const numberQuantity = document.getElementById("num");
+const overlay = document.querySelector(".overlay");
+const closePopup = document.querySelector(".close-popup");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+const accToCartBtn = document.querySelector(".btn-With-icon");
+const CartPPopupContainer = document.getElementById("cart-popup");
+const cartIcon = document.getElementById("cart-icon");
